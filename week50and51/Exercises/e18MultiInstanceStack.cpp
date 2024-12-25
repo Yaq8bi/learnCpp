d844a3bc55b21a599af8ec47ca304281d9d40eee
Ahmet Sami <76931121+Yaq8bi@users.noreply.github.com>
2024-12-25 18:50:17 +0100

    Stack Destroy not finished.

--- week50and51/Exercises/e18MultiInstanceStack.cpp
+++ week50and51/Exercises/e18MultiInstanceStack.cpp
@@ -5,7 +5,6 @@ A function to create a stack.
 A function to push a data element a stack.
 A function to get number of the data elements stored in a stack.
 A function to pop an element from a stack.
-
 A function to clear a stack
 A function to destroy a stack
 */
@@ -19,8 +18,8 @@ int top{-1};
 
 int *createTheStack() // create a pointer in the memeory that points to a set of array of fixed size.
 {
-    static int theStack[fixedSize];
-    return theStack;
+    // If new cannot allocate mem, it will return a nullptr, which i will use for checking
+    return new (std::nothrow) int[fixedSize](); // allocate memory and initialize it to 0.
 }
 
 bool push(int *_stack, int _data)
@@ -49,63 +48,109 @@ void pop(int *_stack)
     -1(empty)
     */
 
-    assert(top != -1); // return true to continue, else quit!
-        if (top > -1) // because once we added something, top will be 0.
-        {
-            std::cout << "POPED: " << _stack[top] << std::endl;
-            top--;
+    if (top > -1) // because once we added something, top will be 0.
+    {
+        std::cout << "POPED: " << _stack[top] << std::endl;
+        top--;
 
-            std::this_thread::sleep_for(std::chrono::milliseconds(400));
-        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(400));
+    }
 }
 
-void printAmount()
+void printAmount(int *_stack)
 {
-    int loadingAnim[] = {'L', 'o', 'a', 'd', 'i', 'n', 'g', '.', '.', '.', '.', '.', '.', '.', '.', '.'};
+    int loadingAnim[] = {'L', 'o', 'a', 'd', 'i', 'n', 'g', '.', '.', '.', '.', '.', '.'};
 
-    for (int i = 0; i < 16; i++)
+    for (int i = 0; i < 13; i++)
     {
         printf("%c", loadingAnim[i]);
         std::this_thread::sleep_for(std::chrono::milliseconds(300));
     }
 
-    if (top == -1)
+    if (!_stack)
     {
-        /* code */
-        printf("\v\bThe stack is empty!");
+        printf("\nThe stack does not exist in memory!");
+        std::cout.flush();
+        return;
     }
 
-    printf("\nThere are %i amount of items in the stack.\n", top);
+    if (top >= 0 && top <= fixedSize)
+    {
+        printf("\nThere are %i amount of items in the stack.\n", top+1);
+        std::cout.flush();
+    }
+    else
+    {
+        printf("\nThe stack is empty!");
+        std::cout.flush();
+    }
 }
 
 void clear(int *_stack)
 {
-    assert(top >= 0);
-    for (int i = 0; i < fixedSize; i++)
+    if (top >= 0)
     {
-        _stack[top] = 0;
-        top--;
+        for (int i = 0; i < fixedSize; i++)
+        {
+            _stack[top] = 0;
+            top--;
+        }
+        top = -1;
+        printf("\nThe stack has been reset!");
+        std::cout.flush();
     }
-    printf("The stack has been cleared!");
+    else
+    {
+        printf("\nCould not clear, stack is empty.");
+        std::cout.flush();
+    }
+}
+
+void destroyStack(int **_stack) // double pointer, because the stack in main(argument) is also a pointer from another function.
+{
+    if (*_stack) // single pointer to the pointer in main.
+    {
+        delete[] *_stack;
+        *_stack = nullptr;
+        std::cout << "\nThe stack is destroyed!" << std::endl;
+        std::cout.flush();
+    }
+    else
+    {
+        std::cout << "The stack does NOT EXIST!" << std::endl;
+        std::cout.flush();
+    }
 }
 
 int main()
 {
-    int *theStack = createTheStack(); // an integer pointer that points to the static pointer array that is returned from the function.
+    int *theStack = createTheStack(); // an integer pointer that points to a dynamically allocated pointer of the amount of "fixedSize".
     int data{0};                      // initialize data with 0
     int items{0};
 
     for (size_t i = 0; i < fixedSize; i++)
     {
         std::cout << "\vWhat would you like to save?:" << std::endl;
-        std::cin >> data;                                                               // take data from mr user
-        printf("The push was %s ", push(theStack, data) ? "SUCCESS!\n" : "FAILURE!\n"); // This broke the code after a boolean was returned.
+        std::cin >> data;                                                                 // take data from mr user
+        printf("\nThe push was %s ", push(theStack, data) ? "SUCCESS!\n" : "FAILURE!\n"); // This broke the code after a boolean was returned.
+        std::cout.flush();
     }
 
-    printAmount();
+    assert(top != -1); // return true to continue, else quit!
+    printAmount(theStack);
+
     pop(theStack); // print the LIFO(last in first out)!
-    printAmount();
-    clear(theStack);//if top is more than 0, clear. 
+
+    assert(top != -1); // return true to continue, else quit! check if top is not -1(empty)
+    printAmount(theStack);
+
+    assert(top >= 0);
+    clear(theStack); // if top is more than 0, clear.
+
+    destroyStack(&theStack);
+    assert(theStack == nullptr);
+
+    printAmount(theStack);
 
     return 0;
 }
